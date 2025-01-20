68e16ba0eaa663c9c4638fd408f8b99a90f06df4
Jordon Bigelow <the-music-dude@hotmail.com>
2025-01-20 16:47:27 -0700

    changed movement to be synced with delta for consistent movement regardless of framerate

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,21 +1,22 @@
 extends RigidBody2D
 
-const SPEED: float = .1
+const SPEED: float = 250
 var velocity: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity = Vector2(-SPEED, 0.1)
+	velocity = Vector2(-SPEED, randi_range(100, 150))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var collision := move_and_collide(velocity)
+	var scaled_velocity = velocity * delta
+	var collision := move_and_collide(scaled_velocity)
 	
 	if collision:
 		if collision.get_collider_id() == 30198990121:
-			print("Game Over")
+			print("Game Over, Player 2 Wins")
 			get_tree().paused = true
 		elif collision.get_collider_id() == 30232544555:
-			print("Game Over")
+			print("Game Over, Player 1 Wins")
 			get_tree().paused = true
 		if collision.get_collider_id() == 29964109068:
 			Globals.player_1_score += 1
