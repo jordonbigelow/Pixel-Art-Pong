06d164b5e366ef5a331c616427060e2bcaea86b2
Jordon Bigelow <the-music-dude@hotmail.com>
2025-01-28 12:26:36 -0700

    moved all code related to the timer functionality into this scene script where it belongs

---
+++ scripts/reset_timer.gd
@@ -0,0 +1,23 @@
+extends Timer
+
+signal started
+
+@onready var countdown: String = $Label.text
+
+
+func _on_timeout() -> void:
+	$Label.visible = false
+	get_tree().reload_current_scene()
+
+
+func _on_started() -> void:
+	$Label.visible = true 
+	
+	
+func reset_timer() -> void:
+	start(3.0)
+	started.emit()
+
+
+func _process(_delta: float) -> void:
+	$Label.text = "Restart in... " + str(round($".".time_left))
