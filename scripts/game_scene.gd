e5a389533bce7a33aa5b7887bded53a5b95743a1
Jordon Bigelow <the-music-dude@hotmail.com>
2025-01-28 15:05:13 -0700

    added soundfx and code to establish when a player has won the game

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -5,6 +5,9 @@ extends Node
 @onready var player_1_score: Label = $"Player 1 Score"
 @onready var player_2_score: Label = $"Player 2 Score"
 
+signal player_1_won
+signal player_2_won
+
 
 func _ready() -> void:
 	player_1_paddle.texture = load("res://assets/paddles/p1_paddle.png")
@@ -25,16 +28,44 @@ func _on_reset_button_pressed() -> void:
 func _on_player_one_back_wall_body_entered(body: Node2D) -> void:
 	Globals.player_2_score += 1
 	body.queue_free()
-	$ResetTimer._reset_timer()
+	
+	if check_if_player_won(Globals.player_1_score, Globals.player_2_score):
+		player_1_won.emit()	
+	else:
+		$ResetTimer._reset_timer()
+		
 	play_scored_sound()
 	
 
 func _on_player_two_back_wall_body_entered(body: Node2D) -> void:
 	Globals.player_1_score +=1
 	body.queue_free()
-	$ResetTimer._reset_timer()
+	
+	if check_if_player_won(Globals.player_1_score, Globals.player_2_score):	
+		player_1_won.emit()
+	else:
+		$ResetTimer._reset_timer()
+		
 	play_scored_sound()
 	
 	
 func play_scored_sound() -> void:
-	$AudioStreamPlayer2D.play(1.20)
+	$PlayerScored.play(1.20)
+	
+	
+func check_if_player_won(p1_score: int, p2_score: int) -> bool:
+	if p1_score >= Globals.MAX_SCORE and p1_score > p2_score + 2:
+		return true
+	if p2_score >= Globals.MAX_SCORE and p2_score > p1_score + 2:
+		return true
+	else:
+		return false
+
+
+func _on_player_1_won() -> void:
+	$PlayerWon.play(0.2)
+	#get_tree().paused = true
+
+func _on_player_2_won() -> void:
+	$PlayerWon.play(0.2)
+	#get_tree().paused = true
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -2,3 +2,4 @@ extends Node
 
 var player_1_score: int = 0
 var player_2_score: int = 0
+const MAX_SCORE = 11
