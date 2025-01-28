192d016b71bec642d8b8fade55783a50285377ac
Jordon Bigelow <the-music-dude@hotmail.com>
2025-01-27 19:02:01 -0700

    added signals for when the ball enters the back area behind the player paddles

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -13,16 +13,13 @@ func _physics_process(delta: float) -> void:
 	var collision: KinematicCollision2D = move_and_collide(scaled_velocity)
 	
 	if collision:
-		if collision.get_collider_id() == 30198990121:
-			print("Game Over, Player 2 Wins")
-			get_tree().paused = true
-		elif collision.get_collider_id() == 30232544555:
-			print("Game Over, Player 1 Wins")
-			get_tree().paused = true
-		if collision.get_collider_id() == 29964109068:
-			Globals.player_1_score += 1
-		elif collision.get_collider_id() == 30014440717:
-			Globals.player_2_score += 1
-			
 		var normal: Vector2 = collision.get_normal()
 		velocity = velocity.bounce(normal)
+
+
+func _on_player_one_back_wall_body_entered(body: Node2D) -> void:
+	Globals.player_2_score += 1
+
+
+func _on_player_two_back_wall_body_entered(body: Node2D) -> void:
+	Globals.player_1_score += 1
